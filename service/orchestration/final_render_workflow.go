fda0e79698f97da013156b76d8813d814f465c16
Ernie Castro <erniemakesstuff@gmail.com>
2024-10-06 18:34:37 -0700

    Updating comments for final_render_workflow

--- service/orchestration/final_render_workflow.go
+++ service/orchestration/final_render_workflow.go
@@ -14,14 +14,12 @@ func (s *FinalRenderWorkflow) GetWorkflowName() string {
 func (s *FinalRenderWorkflow) Run(ledgerItem tables.Ledger, processId string) error {
 	// TODO:
 	// Select PublishEvent ASSIGNED w/o RENDERING state recorded.
-	// Verify child media is ready.
-	// If Root mediaEvent distribution channel is TEXT-ONLY, set state to RENDERING.
-	//	Set IsFinalRender to true
-	// 	Skip other final-rendering steps.
+	// Verify child media is ready, or Root mediaEvent distribution channel is has no children
 	// Spawn child MediaEvent (singular!) for final-rendering
 	// 	Final rendering should specify all visual and audio resources in-order.
 	// 	Final rendering MediaEvent should include watermark info
 	// 	Set IsFinalRender to true for media events.
+	// Append status RENDERING
 	// Don't need to wait; rely on reaper-workflow to invalidate the PublishEvent.
 	return nil
 }
