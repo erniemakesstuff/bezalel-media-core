9df7654eb8017ffd263e8d48ed2fc8e0b2c7afd3
Ernie Castro <erniemakesstuff@gmail.com>
2024-11-25 15:35:57 -0800

    stubbing farm scaler with capacity provider definition

--- service/scaling/ecs_wrapper.go
+++ service/scaling/ecs_wrapper.go
@@ -33,7 +33,6 @@ func executeScaling(clusterName *string, desiredTasks *int64, taskDefinition *st
 	}
 
 	if shouldScaleDown {
-
 		scaleDownDeltaIndex := int64(len(runningTaskIds)) - *desiredTasks
 		return stopTasks(clusterName, runningTaskIds[:scaleDownDeltaIndex])
 	} else {
@@ -58,16 +57,30 @@ func stopTasks(clusterName *string, runningTaskIds []*string) error {
 func runTasks(clusterName *string, desiredTasks *int64, taskDefinition *string, runningTaskIds []*string) error {
 	referenceId := uuid.New().String()
 	startedBy := "EcsScaleDaemonProcess"
-	_, err := ecs_svc.RunTask(&ecs.RunTaskInput{ // TODO: Set as CapacityProviderStrategy for gpu and ec2 general purpose.
-		LaunchType:     aws.String(ecs.LaunchTypeFargate), // TODO: Update this for dedicated EC2 launchType. Should automatically launch instances.
+	capacityProviderName := "G4dnXlarge"
+	if *taskDefinition == "" { // TODO set this to the core-service task name
+		capacityProviderName = "t4-gen-pub-ip"
+	}
+
+	tasksToSpinUp := *desiredTasks - int64(len(runningTaskIds))
+
+	_, err := ecs_svc.RunTask(&ecs.RunTaskInput{
+		CapacityProviderStrategy: []*ecs.CapacityProviderStrategyItem{
+			{
+				Base:             aws.Int64(tasksToSpinUp),
+				CapacityProvider: aws.String(capacityProviderName),
+				Weight:           aws.Int64(100),
+			},
+		},
 		Cluster:        clusterName,
-		Count:          desiredTasks,
+		Count:          aws.Int64(tasksToSpinUp),
 		TaskDefinition: taskDefinition,
 		ReferenceId:    &referenceId,
 		StartedBy:      &startedBy,
 		NetworkConfiguration: &ecs.NetworkConfiguration{
 			AwsvpcConfiguration: &ecs.AwsVpcConfiguration{
-				AssignPublicIp: aws.String(ecs.AssignPublicIpEnabled), // This is set for Fargate: pulling private ECR needs public IP. Not sure if needed for dedicated EC2.
+				// This is set for Fargate: pulling private ECR needs public IP. Not sure if needed for dedicated EC2.
+				AssignPublicIp: aws.String(ecs.AssignPublicIpEnabled),
 				Subnets: []*string{
 					aws.String("subnet-0f54e9427db2e5713"), // TODO: move this to env configs.
 					aws.String("subnet-03159465d863b0753"),
