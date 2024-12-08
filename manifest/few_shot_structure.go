9252428357ce3784cbffab77472ad5f5c4751933
Ernie Castro <erniemakesstuff@gmail.com>
2024-12-07 19:53:48 -0800

    adjusting time constraints in few shot prompt

--- manifest/few_shot_structure.go
+++ manifest/few_shot_structure.go
@@ -99,12 +99,12 @@ func GetShortVideoJson() string {
 		and that is related to the videoTitle and videoDescription.`,
 		MainPost: `Main post content, summary, or abridged text goes here.
 		If the post is longer than one paragraph long, then abridge the contents to be less than one paragraph; summarizing to capture the main dramatic details.
-		All text in json:mainPost should be at most 45 seconds long when read aloud.`,
+		All text in json:mainPost should be at most 50 seconds long when read aloud.`,
 		Comments: []string{
 			"Comments from the post go here, summarized, or abridged.",
 			"One comment per list entry.",
 			"Select comments that are no more than three sentences long.",
-			"All entries combined in json:comments should only require at most 15 seconds to read aloud.",
+			"All entries combined in json:comments should only require at most 10 seconds to read aloud.",
 		},
 	}
 
